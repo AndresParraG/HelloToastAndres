8eeaab762900099a693fa0d0e4da75c717ababb9
Andres <aparrag@cifpfbmoll.eu>
2021-10-22 12:37:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hellotoastandres/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hellotoastandres;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button countB;
+    private Button toastB;
+    private int num = 0;
+    private TextView showCount = (TextView) findViewById(R.id.show_count);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        countB = (Button) findViewById(R.id.button_count);
+        countB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                num++;
+                updateCounter();
+            }
+        });
+
+        toastB = (Button) findViewById(R.id.button_toast);
+        toastB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), showCount.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateCounter() {
+        showCount.setText(num);
+    }
+}
